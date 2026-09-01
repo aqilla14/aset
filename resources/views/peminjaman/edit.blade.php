fe85e049b7fa49345ea9fe82459a3e56d3cb3d12
cahyaavilia <cahya.avilia43@smk.belajar.id>
2026-09-01 14:18:29 +0700

    memperbaiki peminjaman (cahya)

--- resources/views/peminjaman/edit.blade.php
+++ resources/views/peminjaman/edit.blade.php
@@ -56,322 +56,103 @@
 
 
 
-            <div class="form-group">
+           <div class="form-group">
 
+    <label>Nama Peminjam</label>
 
-                <label>
-                    Nama Peminjam
-                </label>
+    <input type="text"
+           name="peminjam"
+           class="form-control @error('peminjam') is-invalid @enderror"
+           value="{{ old('peminjam', $peminjaman->peminjam) }}"
+           placeholder="Masukkan nama peminjam">
 
+    @error('peminjam')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 
+</div>
 
-                <input type="text"
-                       name="nama_peminjam"
-                       class="form-control @error('nama_peminjam') is-invalid @enderror"
-                       value="{{ old('nama_peminjam', $peminjaman->nama_peminjam) }}"
-                       placeholder="Masukkan nama peminjam">
 
+<div class="form-group">
 
+    <label>NIP / NIM</label>
 
-                @error('nama_peminjam')
+    <input type="text"
+           name="nip_nim"
+           class="form-control @error('nip_nim') is-invalid @enderror"
+           value="{{ old('nip_nim', $peminjaman->nip_nim) }}"
+           placeholder="Masukkan NIP / NIM">
 
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+    @error('nip_nim')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 
-                @enderror
+</div>
 
 
+<div class="form-group">
 
-            </div>
+    <label>Pilih Aset</label>
 
+    <select name="aset_id"
+            class="form-control @error('aset_id') is-invalid @enderror">
 
+        <option value="">-- Pilih Aset --</option>
 
+        @foreach($asets as $aset)
+            <option value="{{ $aset->id }}"
+                {{ old('aset_id', $peminjaman->aset_id) == $aset->id ? 'selected' : '' }}>
+                {{ $aset->nama_aset }}
+            </option>
+        @endforeach
 
+    </select>
 
+    @error('aset_id')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 
+</div>
 
-            <div class="form-group">
 
+<div class="form-group">
 
-                <label>
-                    Pilih Aset
-                </label>
+    <label>Tanggal Pinjam</label>
 
+    <input type="date"
+           name="tanggal_pinjam"
+           class="form-control @error('tanggal_pinjam') is-invalid @enderror"
+           value="{{ old('tanggal_pinjam', $peminjaman->tanggal_pinjam) }}">
 
+    @error('tanggal_pinjam')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 
-                <select name="aset_id"
-                        class="form-control @error('aset_id') is-invalid @enderror">
+</div>
 
 
+<div class="form-group">
 
-                    <option value="">
-                        -- Pilih Aset --
-                    </option>
+    <label>Tanggal Kembali</label>
 
+    <input type="date"
+           name="tanggal_kembali_rencana"
+           class="form-control @error('tanggal_kembali_rencana') is-invalid @enderror"
+           value="{{ old('tanggal_kembali_rencana', $peminjaman->tanggal_kembali_rencana) }}">
 
-
-
-                    @foreach($asets as $aset)
-
-
-                        <option value="{{ $aset->id }}"
-                            
-                            {{ old('aset_id', $peminjaman->aset_id) == $aset->id ? 'selected' : '' }}>
-
-
-                            {{ $aset->nama_aset }}
-
-
-                        </option>
-
-
-
-                    @endforeach
-
-
-
-                </select>
-
-
-
-
-
-                @error('aset_id')
-
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-
-                @enderror
-
-
-
-            </div>
-
-
-
-
-
-
-
-            <div class="form-group">
-
-
-                <label>
-                    Tanggal Pinjam
-                </label>
-
-
-
-                <input type="date"
-                       name="tanggal_pinjam"
-                       class="form-control @error('tanggal_pinjam') is-invalid @enderror"
-                       value="{{ old('tanggal_pinjam', $peminjaman->tanggal_pinjam) }}">
-
-
-
-
-
-                @error('tanggal_pinjam')
-
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-
-                @enderror
-
-
-
-            </div>
-
-
-
-
-
-
-
-
-            <div class="form-group">
-
-
-                <label>
-                    Tanggal Kembali
-                </label>
-
-
-
-                <input type="date"
-                       name="tanggal_kembali"
-                       class="form-control @error('tanggal_kembali') is-invalid @enderror"
-                       value="{{ old('tanggal_kembali', $peminjaman->tanggal_kembali) }}">
-
-
-
-
-
-                @error('tanggal_kembali')
-
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-
-                @enderror
-
-
-
-            </div>
-
-
-
-
-
-
-
-
-            <div class="form-group">
-
-
-                <label>
-                    Jumlah
-                </label>
-
-
-
-                <input type="number"
-                       name="jumlah"
-                       class="form-control @error('jumlah') is-invalid @enderror"
-                       value="{{ old('jumlah', $peminjaman->jumlah) }}"
-                       min="1">
-
-
-
-
-
-                @error('jumlah')
-
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-
-                @enderror
-
-            </div>
-
-            <div class="form-group">
-
-
-                <label>
-                    Status
-                </label>
-
-
-
-                <select name="status" class="form-control">
-
-
-                    <option value="Dipinjam"
-                        {{ old('status', $peminjaman->status) == 'Dipinjam' ? 'selected' : '' }}>
-
-                        Dipinjam
-
-                    </option>
-
-
-
-                    <option value="Selesai"
-                        {{ old('status', $peminjaman->status) == 'Selesai' ? 'selected' : '' }}>
-
-                        Selesai
-
-                    </option>
-
-
-
-                </select>
-
-
-
-            </div>
-
-
-
-
-
-
-
-
-
-            <div class="form-group">
-
-
-                <label>
-                    Keterangan
-                </label>
-
-
-
-                <textarea name="keterangan"
-                          class="form-control"
-                          rows="3">{{ old('keterangan', $peminjaman->keterangan) }}</textarea>
-
-
-
-            </div>
-
-
-
-
-
-
-
-
-
-            <div class="form-group mt-3">
-
-
-
-                <a href="{{ route('peminjaman.index') }}"
-                   class="btn btn-secondary">
-
-
-                    <i class="fas fa-arrow-left mr-1"></i>
-
-                    Kembali
-
-
-                </a>
-
-
-
-
-
-
-                <button type="submit"
-                        class="btn btn-warning">
-
-
-                    <i class="fas fa-save mr-1"></i>
-
-                    Update
-
-
-                </button>
-
-
-
-            </div>
-
-
-
-
-
-        </form>
-
-
-
-    </div>
-
+    @error('tanggal_kembali_rencana')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 
 </div>
 
